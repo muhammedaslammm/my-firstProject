74335cccc68b955ad76119b83565179a353fd096
Muhammed Aslam <muhammedbinramli@gmail.com>
2024-04-17 18:23:31 +0530

    added name

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -27,7 +27,7 @@ exports.placeOrder = async function(req,res){
         const productIDs = carts.map(function(cart){
             return cart.productID._id;
         })
-        // dhdhjdk
+        const name = 'muhammed aslam';
         const orderedProducts = [];
         const date = new Date();
         const newDate = date.setDate(date.getDate() + 3)
